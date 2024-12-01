a1c8477e2740452c2de22b10ce20efd07b7139a2
oldshifoner <oldshifoner@gmail.com>
2024-12-01 11:59:47 +0700

    Initial Commit

---
+++ NetworkStorage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkStorage
+//
+//  Created by Максим Игоревич on 01.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        testInit
+    }
+
+
+}
+
