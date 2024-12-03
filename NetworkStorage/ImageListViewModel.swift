f71ffe40649c5cf6cd60b14505af22f2637628af
oldshifoner <oldshifoner@gmail.com>
2024-12-03 22:05:53 +0700

    111

--- NetworkStorage/ImageListViewModel.swift
+++ NetworkStorage/ImageListViewModel.swift
@@ -16,12 +16,16 @@ class ImageListViewModel: ObservableObject {
     func fetchImages() {
         // Имитация получения данных с сервера
         let mockData = [
-            ImageModel(id: "1", url: "https://example.com/image1.jpg"),
-            ImageModel(id: "2", url: "https://example.com/image2.jpg")
+            ImageModel(id: "1", url: "http://164.90.163.215:1337/uploads/thumbnail_FD_046_BA_8_298_F_4946_9_C13_D3_CE_54_F1_E7_B3_8483506438.jpg"),
+            ImageModel(id: "2", url: "http://164.90.163.215:1337/uploads/thumbnail_1746_E983_9793_492_A_86_A4_E3_CDB_7_A4_D911_6a52ef8620.jpg"),
+            ImageModel(id: "3", url: "http://164.90.163.215:1337/uploads/thumbnail_FD_046_BA_8_298_F_4946_9_C13_D3_CE_54_F1_E7_B3_8483506438.jpg"),
+            ImageModel(id: "4", url: "http://164.90.163.215:1337/uploads/thumbnail_1746_E983_9793_492_A_86_A4_E3_CDB_7_A4_D911_6a52ef8620.jpg"),
+            ImageModel(id: "5", url: "http://164.90.163.215:1337/uploads/thumbnail_1746_E983_9793_492_A_86_A4_E3_CDB_7_A4_D911_6a52ef8620.jpg")
+            
         ]
         self.images = mockData
     }
-
+    
     func downloadImage(for image: ImageModel) {
         guard let url = URL(string: image.url) else { return }
         
